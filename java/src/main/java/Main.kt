d406f3fdc8a56f12a7a21a6df19a7e6aab73eac5
Ebn Zhang <ebnbin@gmail.com>
2021-03-05 02:47:15 +0800

    Test OpenMoji

--- java/src/main/java/Main.kt
+++ java/src/main/java/Main.kt
@@ -1,2 +1,15 @@
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
+
 fun main() {
+//    json()
+}
+
+private fun json() {
+    val openMojiList: List<OpenMoji> = Gson().fromJson(
+        File("file", "openmoji.json").bufferedReader(),
+        object : TypeToken<List<OpenMoji>>() {}.type,
+    )
+    println(openMojiList.size)
 }
---
+++ java/src/main/java/OpenMoji.kt
@@ -0,0 +1,39 @@
+import com.google.gson.GsonBuilder
+
+data class OpenMoji(
+    val emoji: String,
+    val hexcode: String,
+    val group: String,
+    val subgroups: String,
+    val annotation: String,
+    val tags: String,
+    val openmoji_tags: String,
+    val openmoji_author: String,
+    val openmoji_date: String,
+    val skintone: String,
+    val skintone_combination: String,
+    val skintone_base_emoji: String,
+    val skintone_base_hexcode: String,
+    val unicode: String,
+    val order: String,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as OpenMoji
+        if (hexcode != other.hexcode) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return hexcode.hashCode()
+    }
+
+    fun toJson(prettyPrinting: Boolean = true): String {
+        return GsonBuilder().also {
+            if (prettyPrinting) {
+                it.setPrettyPrinting()
+            }
+        }.create().toJson(this)
+    }
+}
