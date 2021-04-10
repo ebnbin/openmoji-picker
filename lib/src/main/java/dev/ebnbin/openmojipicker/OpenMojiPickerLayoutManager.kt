73cbe2968c2e22e2fa698a61bcb8eaae6c397911
Ebn Zhang <ebnbin@gmail.com>
2021-04-11 02:51:11 +0800

    Update OpenMojiPicker icon size

--- lib/src/main/java/dev/ebnbin/openmojipicker/OpenMojiPickerLayoutManager.kt
+++ lib/src/main/java/dev/ebnbin/openmojipicker/OpenMojiPickerLayoutManager.kt
@@ -11,7 +11,7 @@ internal class OpenMojiPickerLayoutManager(
 ) : SpanSizeGridLayoutManager(
     context,
     viewModel,
-    spanSize = 56f,
+    spanSize = 48f,
 ) {
     override fun onAttachedToWindow(view: RecyclerView?) {
         super.onAttachedToWindow(view)
